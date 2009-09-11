62c8cdd8a2f077709a2bdc68ff04d8e94ceaa162
Richard <richard@richard-laptop.(none)>
2009-09-11 15:43:01 -0700

    does nothing but doesn't fail

--- sc_utils/management/commands/create_sproutcore_schema.py
+++ sc_utils/management/commands/create_sproutcore_schema.py
@@ -1,52 +1,93 @@
-from django.core.management.base import AppCommand, CommandError
+from django.core.management.base import NoArgsCommand
 from django.core.management.color import no_style
+from django.utils.importlib import import_module
 from optparse import make_option
+import sys
 
-class Command(AppCommand):
-    option_list = AppCommand.option_list + (
+try:
+    set
+except NameError:
+    from sets import Set as set   # Python 2.3 fallback
+
+class Command(NoArgsCommand):
+    option_list = NoArgsCommand.option_list + (
         make_option('--noinput', action='store_false', dest='interactive', default=True,
             help='Tells Django to NOT prompt the user for input of any kind.'),
     )
-    help = "Executes ``sqlreset`` for the given app(s) in the current database."
-    args = '[appname ...]'
+    help = "Create the sprout core models for all apps in INSTALLED_APPS \
+            whose models haven't already been created. For those already \
+            created, subclass files will not be overwritten."
 
-    output_transaction = True
-
-    def handle_app(self, app, **options):
-        from django.db import connection, transaction
+    def handle_noargs(self, **options):
+        from django.db import connection, transaction, models
         from django.conf import settings
-        from django.core.management.sql import sql_reset
+        # from django.core.management.sql import custom_sql_for_model, emit_post_sync_signal
 
-        app_name = app.__name__.split('.')[-2]
+        verbosity = int(options.get('verbosity', 1))
+        interactive = options.get('interactive')
+        show_traceback = options.get('traceback', False)
 
         self.style = no_style()
 
-        sql_list = sql_reset(app, self.style)
-
-        if options.get('interactive'):
-            confirm = raw_input("""
-You have requested a database reset.
-This will IRREVERSIBLY DESTROY any data for
-the "%s" application in the database "%s".
-Are you sure you want to do this?
-
-Type 'yes' to continue, or 'no' to cancel: """ % (app_name, settings.DATABASE_NAME))
-        else:
-            confirm = 'yes'
-
-        if confirm == 'yes':
+        # Import the 'management' module within each installed app, to register
+        # dispatcher events.
+        for app_name in settings.INSTALLED_APPS:
             try:
-                cursor = connection.cursor()
-                for sql in sql_list:
-                    cursor.execute(sql)
-            except Exception, e:
-                transaction.rollback_unless_managed()
-                raise CommandError("""Error: %s couldn't be reset. Possible reasons:
-  * The database isn't running or isn't configured correctly.
-  * At least one of the database tables doesn't exist.
-  * The SQL was invalid.
-Hint: Look at the output of 'django-admin.py sqlreset %s'. That's the SQL this command wasn't able to run.
-The full error: %s""" % (app_name, app_name, e))
-            transaction.commit_unless_managed()
-        else:
-            print "Reset cancelled."
+                import_module('.management', app_name)
+            except ImportError, exc:
+                # This is slightly hackish. We want to ignore ImportErrors
+                # if the "management" module itself is missing -- but we don't
+                # want to ignore the exception if the management module exists
+                # but raises an ImportError for some reason. The only way we
+                # can do this is to check the text of the exception. Note that
+                # we're a bit broad in how we check the text, because different
+                # Python implementations may not use the same text.
+                # CPython uses the text "No module named management"
+                # PyPy uses "No module named myproject.myapp.management"
+                msg = exc.args[0]
+                if not msg.startswith('No module named') or 'management' not in msg:
+                    raise
+                print app_name
+
+#        cursor = connection.cursor()
+
+        # Get a list of already installed *models* so that references work right.
+#        tables = connection.introspection.table_names()
+#        seen_models = connection.introspection.installed_models(tables)
+#TODO: check for files here
+        created_models = set()
+        pending_references = {}
+
+        # Create the tables for each model
+        for app in models.get_apps():
+            app_name = app.__name__.split('.')[-2]
+            model_list = models.get_models(app)
+            for model in model_list:
+                # Create the model's database table, if it doesn't already exist.
+                if verbosity >= 2:
+                    print "Processing %s.%s model" % (app_name, model._meta.object_name)
+#                seen_models.add(model)#TODO: Change to file list
+                created_models.add(model)
+                if verbosity >= 1:
+                    print "Creating file for %s" % model._meta.db_table
+
+        # Create the m2m tables. This must be done after all tables have been created
+        # to ensure that all referred tables will exist.
+#        for app in models.get_apps():
+#            app_name = app.__name__.split('.')[-2]
+#            model_list = models.get_models(app)
+#            for model in model_list:
+#                if model in created_models:
+#                    if True:
+#                        if verbosity >= 2:
+#                            print "Creating many-to-many tables for %s.%s model" % (app_name, model._meta.object_name)
+
+        # Send the post_syncdb signal, so individual apps can do whatever they need
+        # to do at this point.
+#        emit_post_sync_signal(created_models, verbosity, interactive)
+
+
+        # Install the 'initial_data' fixture, using format discovery
+#        from django.core.management import call_command
+#        call_command('loaddata', 'initial_data', verbosity=verbosity)
+
