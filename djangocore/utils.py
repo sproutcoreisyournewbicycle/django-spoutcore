26951fe64526c11bbfb9d6360154d7bcf6d8accf
Taavi Taijala <taavi@taijala.com>
2009-09-12 11:05:20 -0700

    Added is_ajax decorator to utils.py

--- djangocore/utils.py
+++ djangocore/utils.py
@@ -1,5 +1,26 @@
 import re
 
+from django.http import HttpResponseBadRequest
+from django.conf import settings
+
+def ajax_required(f):
+    """
+    AJAX request required decorator
+    use it in your views:
+
+    @ajax_required
+    def my_view(request):
+        ....
+
+    """    
+    def wrap(request, *args, **kwargs):
+            if not request.is_ajax() and not settings.DEBUG:
+                return HttpResponseBadRequest
+            return f(request, *args, **kwargs)
+    wrap.__doc__=f.__doc__
+    wrap.__name__=f.__name__
+    return wrap
+
 def camelize(string):
     """
     Returns given string as CamelCased.
