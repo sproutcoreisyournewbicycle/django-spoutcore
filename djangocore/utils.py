23cc48d835093e60861ee90a87c555574ea390ae
Taavi Taijala <taavi@taijala.com>
2009-09-17 13:26:58 -0700

    Added a sproutcore-specific JSONEncoder class which understands django's translations objects. Added a camelize function which returns a camelized name with the first letter in lowercase. Updated all string functions in utils.py to be able to accept django's translation objects.

--- djangocore/utils.py
+++ djangocore/utils.py
@@ -1,6 +1,25 @@
 import re
 from django.http import HttpResponseBadRequest, HttpResponseNotAllowed
 from django.conf import settings
+from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.functional import Promise
+from django.utils.encoding import smart_str
+
+class SproutCoreJSONEncoder(DjangoJSONEncoder):
+    """
+    JSONEncoder subclass that knows how to deal with Django translation
+    objects.
+    """
+    def default(self, o):
+        if isinstance(o, Promise):
+            return smart_str(o)
+
+        try:
+            return super(SproutCoreJSONEncoder, self).default(o)
+
+        # If we get something that can't be encoded, then we just encode None.
+        except TypeError:
+            return None
 
 def ajax_required(func):
     """
@@ -96,7 +115,23 @@ def camelize(string):
     be converted to "WhoSOnline"
     
     """
+    string = smart_str(string)
     return ''.join(w[0].upper() + w[1:] for w in re.sub('[^A-Z^a-z^0-9^:]+', ' ', string).split(' ') if w)
+
+def lcamelize(string):
+    """
+    Returns given string as CamelCased, but with the first letter as
+    lowercase.
+    
+    Converts a string like "send_email" to "sendEmail". It will remove
+    non-alphanumeric character from the string, so "who's online" will
+    be converted to "whoSOnline"
+    
+    """
+    string = camelize(string)
+    if string:
+        string = string[0].lower() + string[1:]
+    return string
 
 def underscore(string):
     """
@@ -107,6 +142,7 @@ def underscore(string):
     friendly URLs.
     
     """
+    string = smart_str(string)
     return  re.sub('[^A-Z^a-z^0-9^\/]+','_', \
             re.sub('([a-z\d])([A-Z])','\\1_\\2', \
             re.sub('([A-Z]+)([A-Z][a-z])','\\1_\\2', re.sub('::', '/',string)))).lower()
