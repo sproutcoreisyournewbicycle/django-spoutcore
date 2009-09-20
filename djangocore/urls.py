a71af6cb168e0cb8f149769881a6b5ad1f0ddf83
Taavi Taijala <taavi@taijala.com>
2009-09-20 12:34:58 -0700

    When using the auto admin REST interface, and DEBUG is set to true, piston now returns all responses in plain text. This makes it so you can view responses directly in your browser.

--- djangocore/urls.py
+++ djangocore/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from djangocore.handlers import length_resource, range_resource, bulk_resource, object_resource
 
@@ -8,3 +9,12 @@ urlpatterns = patterns('',
     url(r'^(?P<app_label>[^/]+)/(?P<module_name>[^/]+)/bulk/$', bulk_resource),
     url(r'^(?P<app_label>[^/]+)/(?P<module_name>[^/]+)/$', object_resource),
 )
+
+# Have all emitters send responses in plain text when debugging. This makes it
+# easy to view responses directly in your browser.
+if settings.DEBUG:
+    from piston.emitters import Emitter
+    for key, (emitter, ct) in Emitter.EMITTERS.items():
+        # Replace the content type for all emitters with 'text/plain'.
+        Emitter.EMITTERS[key] = (emitter, 'text/plain')
+            
