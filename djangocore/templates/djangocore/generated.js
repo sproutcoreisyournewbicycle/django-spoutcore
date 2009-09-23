51d5d8001821f6b15968fdb415689713866530b9
Taavi Taijala <taavi@taijala.com>
2009-09-22 23:43:00 -0700

    Changed the templates to meet JSDoc requirements (no underscores in class names, and added private class declaration).

--- djangocore/templates/djangocore/generated.js
+++ djangocore/templates/djangocore/generated.js
@@ -2,8 +2,9 @@
 //WARNING: THIS FILE IS GENERATED AUTOMATICALLY AND MAY BE OVERWRITTEN.
 //IF YOU WISH TO MAKE CHANGES, SUBCLASS THE MODEL AND MAKE CHANGES THERE.
 
-{{ app_label }}._{{ model_name }} = SC.Record.extend(
-/** {{ app_label }}._{{ model_name }}.prototype */ {
+/** @private */
+{{ app_label }}.Generated{{ model_name }} = SC.Record.extend(
+/** {{ app_label }}.{{ model_name }}.prototype */ {
 {% for field in generated_fields %}
 /**
 {{ field.comments }}
--- djangocore/templates/djangocore/user.js
+++ djangocore/templates/djangocore/user.js
@@ -1,7 +1,7 @@
 {% autoescape off %}
 sc_require('{{ generated_file_name }}');
 
-{{ app_label }}.{{ model_name }} = {{ app_label }}._{{ model_name }}.extend(
+{{ app_label }}.{{ model_name }} = {{ app_label }}.Generated{{ model_name }}.extend(
 /** {{ app_label }}.{{ model_name }}.prototype */ {
 
   // Add SproutCore specific code here
